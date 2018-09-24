2a12d6468b521da73c50207b8b542decd76c4ef5
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-24 23:38:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,6 +24,6 @@ WIN_COMBINATIONS = [
       elsif (board[win_combination[0]]) == "O" && (board[win_combination[1]]) == "O" && (board[win_combination[2]]) == "O"
       return win_combination
       end
-    end
   false
+    end
   end 
