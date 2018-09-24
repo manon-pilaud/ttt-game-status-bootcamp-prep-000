67dd529b5818d9b102d0667c80c9977adc3178bf
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-24 23:30:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,6 @@ WIN_COMBINATIONS = [
   def won?(board)
     WIN_COMBINATIONS.each do |win_combination|
       if win_combination[0] != '' && win_combination[1] != '' && win_combination[2] != ''
-        
+        return win_combo
     end
   end 
